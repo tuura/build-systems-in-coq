7b06c87fde39efa799a25a4a5cad588bb1c5a3c5
Alexandre Moine <alexandre@moine.me>
2018-12-03 22:37:18 +0100

    Better Acyclic ?

--- src/Build/AcyclicTask.v
+++ src/Build/AcyclicTask.v
@@ -24,7 +24,7 @@ Inductive Task (C : (Type -> Type) -> Type) (K V : Type) := {
 Arguments run {C} {K} {V} _ {F} {CF}.
 
 Definition AcyclicTasks (C : (Type -> Type) -> Type) (V : Type) :=
-  forall (k : nat), Maybe (Task C (Fin.t k) V).
+  forall (k : nat), Maybe (Task C  ({x:nat | k>x}) V).
 
 Definition depth {C : (Type -> Type) -> Type} {V : Type} {F : (Type -> Type)} `{CF: C F}
            (tasks : AcyclicTasks C V) (key : nat) : nat :=
@@ -33,58 +33,17 @@ Definition depth {C : (Type -> Type) -> Type} {V : Type} {F : (Type -> Type)} `{
   | Just _  => key
   end.
 
-Open Scope monad_scope.
+Require Import Coq.Init.Specif.
 
-(* TODO: use standart lemmas instead of these ad-hoc ones *)
-Lemma succ_gt_zero : forall n, 0 = S n -> False.
-Proof. intros. omega. Qed.
+Lemma ok_k' (k:nat) : S k > k.
+Proof. auto. Qed.
 
-Lemma succ_eq_impls_eq : forall n m, S n = S m -> n = m.
-Proof. intros. omega. Qed.
+Definition ok_k (k : nat) := exist (gt (S k)) k (ok_k' k).
 
-Lemma succsucc_gt_zero : forall n, 0 = S (S n) -> False.
-Proof. intros. omega. Qed.
+Lemma ok_kk' (k:nat) : S (S k) > k.
+Proof. auto. Qed.
 
-(* Safely convert a (p : nat) into a Fin.t n if n = S p *)
-Fixpoint of_nat_succ {p n : nat} : n = S p -> t n :=
-  match n with
-    |0 => fun H : 0 = S p => False_rect _ (succ_gt_zero p H)
-    |S n' => match p with
-      |0 => fun _ => @F1 n'
-      |S p' => fun H => FS (of_nat_succ (succ_eq_impls_eq _ _ H))
-    end
-  end.
-
-(* Safely convert a (p : nat) into a Fin.t n if n = S (S p) *)
-(* TODO: reuse of_nat_succ *)
-Fixpoint of_nat_succsucc {p n : nat} : n = S (S p) -> t n :=
-  match n with
-    |0 => fun H : 0 = S (S p) => False_rect _ (succsucc_gt_zero p H)
-    |S n' => match p with
-      |0 => fun _ => @F1 n'
-      |S p' => fun H => FS (of_nat_succsucc (succ_eq_impls_eq _ _ H))
-    end
-  end.
-
-(* Can't come up with a short name for this lemma, so let is just be l *)
-Lemma l : forall {n n' n''},
-    n = S n' -> n' = S n'' -> n = S (S n'').
-Proof. intros. omega. Qed.
-
-Fixpoint from_nat (p : nat) : Fin.t (S p) :=
-  match p with
-  |0 => F1
-  |S p' => FS (from_nat p')
-  end.
-
-Fixpoint inject__1 {m} (x : Fin.t m) : Fin.t (S m) :=
-  match x with
-  | F1 => F1
-  | FS x' => FS (inject__1 x')
-  end.
-
-Check Fin.L.
-Print Fin.L.
+Definition ok_kk (k : nat) := exist (gt (S (S k))) k (ok_kk' k).
 
 Definition fibonacci :
   AcyclicTasks Applicative nat := fun n =>
@@ -92,9 +51,9 @@ Definition fibonacci :
   | 0  => Nothing
   | 1  => Nothing
   | S (S m) => Just
-      {| run := fun _ _ => fun fetch =>
-           Nat.add <$> fetch (from_nat (S m))
-                   <*> fetch (inject__1 (from_nat m)) |}
+      {| run := fun _ _ => fun fetch => 
+           Nat.add <$> fetch (ok_k (S m))
+                   <*> fetch (ok_kk (m)) |}
   end.
 
 (* -- | Find the dependencies of an applicative task. *)
@@ -113,10 +72,9 @@ Definition dependencies {K V : Type} (task : Task Applicative K V) : list K :=
 Definition deps_fib (k : nat) : list nat :=
   match fibonacci k with
   | Nothing => nil
-  | Just task => map (fun x => proj1_sig (Fin.to_nat x)) (dependencies task)
+  | Just task => map (fun x => proj1_sig x) (dependencies task)
   end.
 
 Eval compute in deps_fib (S (S 1)).
-
 
 (* dependencies (fib (S (S n)) == [n, S n] *)
