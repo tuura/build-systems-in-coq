706e8818ceadde48540530c267a7ea55ecd1ff16
Georgy Lukyanov <georgiylukjanov@gmail.com>
2018-12-08 17:57:36 +0000

    'Functor', 'Applicative' and 'Monad' instances for 'Identity'.

--- src/Data/Functor/Identity.v
+++ src/Data/Functor/Identity.v
@@ -1,6 +1,25 @@
-Inductive Identity A := Id : A -> Identity A.
+(* The identity functor, applicative and monad. *)
 
-Definition getIdentity {A : Type} (a : Identity A) :=
-  match a with
-  | Id _ x => x
-  end.
+Require Import Data.Functor.
+Require Import Control.Applicative.
+Require Import Control.Monad.
+
+Inductive Identity A := mkIdentity {runIdentity : A}.
+
+Arguments mkIdentity {_} _.
+Arguments runIdentity {_} _.
+
+Instance Identiry_Functor : Functor Identity :=
+  {
+    fmap {_} {_} f x := mkIdentity (f (runIdentity x))
+  }.
+
+Instance Identity_Applicative : Applicative Identity :=
+  {
+    pure _ x := mkIdentity x;
+    ap _ _ f x := mkIdentity ((runIdentity f) (runIdentity x))
+  }.
+
+Instance Identity_Monad : Monad Identity := {
+  join _ x := runIdentity x
+}.
